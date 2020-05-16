18a58e5898053780275d9bd19c78383e3c745bc8
maarten4000 <maarten_of@hotmail.com>
2020-05-16 13:02:51 +0200

    maarten

--- hello.py
+++ hello.py
@@ -1,20 +1,40 @@
 import streamlit as sl
+from PIL import Image
+import pandas as pd
 
 
 
 
+
+@sl.cache
+def Load_data(nrows):
+    df = pd.read_csv(r"C:\Users\maart\OneDrive\Bureaublad\Data\airline_passengers.csv",nrows=nrows,index_col=["Month"],parse_dates=["Month"])
+    print(df.columns)
+    return df
 
 
 def main():
-    sl.title("Tim je bent een sukkel")
+
+    # IN THE SIDEBAR
+    sl.sidebar.slider("Select the year",0,23,12)
+    sl.sidebar.button("Homee!!")
+    sl.title("Airline passengers")
+    sl.markdown("""Predicting of the airline passengers with Time series forecasting 
+                models we used are ARIMA and HOLT WINTER""")
+
+
+    img = Image.open("download.jpg")
+    sl.image(img,width=500)
+
+
+    df = Load_data(1000)
+    sl.write(df)
 
 
 
+    sl.line_chart(df["Thousands of Passengers"])
 
-
-
-
-
+    sl.bar_chart(df["Thousands of Passengers"])
 
 if __name__ =="__main__":
     main()
